8083e086e47d78874fa6382aaaf9bfee09b1415c
zhur4k <kostya.zhurovich12052@gmail.com>
2024-11-25 15:03:16 +0300

    fix: fix repository extends.

--- src/main/java/com/walletapi/repository/WalletRepository.java
+++ src/main/java/com/walletapi/repository/WalletRepository.java
@@ -3,5 +3,7 @@ package com.walletapi.repository;
 import com.walletapi.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WalletRepository extends JpaRepository<Wallet, Long> {
+import java.util.UUID;
+
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 }
