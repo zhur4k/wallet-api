f594f5644869dec693314bd9ec45af21f49f3672
zhur4k <kostya.zhurovich12052@gmail.com>
2024-11-25 14:57:13 +0300

    feat: add getters and setters.

--- src/main/java/com/walletapi/model/Wallet.java
+++ src/main/java/com/walletapi/model/Wallet.java
@@ -4,13 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
 @Table(name = "wallets")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Wallet {
